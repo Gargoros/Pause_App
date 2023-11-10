47f6d8283b5342ac27c9c7c4c170287f651fae19
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2023-11-10 18:55:47 +0300

    Initial Commit

---
+++ Pause_App/Pause_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pause_AppApp.swift
+//  Pause_App
+//
+//  Created by MIKHAIL ZHACHKO on 10.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Pause_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
